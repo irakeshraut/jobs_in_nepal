584e040d113682d35ea7117c1f3785d361c5f311
Rakesh Raut <irakeshraut@gmail.com>
2021-10-27 16:30:38 +1100

    convert Themeforest template into rails. Kept all files inside vendor folder.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../../../vendor/assets/stylesheets .css
+//= link_directory ../../../vendor/assets/javascripts .js
+//= link_tree ../../../vendor/assets/images
+//= link_tree ../../../vendor/assets/fonts
