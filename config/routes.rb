f1fabc664d091ffc24e6bbbc71a33ab323ef2484
Rakesh Raut <irakeshraut@gmail.com>
2021-10-29 20:20:20 +1100

    setup Sorcery and User model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home_pages#index'
+
+  get 'login',  to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+
+  resources :users
+  resources :sessions,  only: [:new, :create, :destroy]
 end
--- db/migrate/20211029065141_sorcery_core.rb
+++ db/migrate/20211029065141_sorcery_core.rb
@@ -1,7 +1,10 @@
 class SorceryCore < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
-      t.string :email,            null: false
+    create_table :users, id: :uuid do |t|
+      t.string :first_name, null: false
+      t.string :last_name,  null: false
+      t.string :role,       null: false
+      t.string :email,      null: false
       t.string :crypted_password
       t.string :salt
 
