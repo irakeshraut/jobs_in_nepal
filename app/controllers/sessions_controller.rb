9d29dd6cf6e47af6c7229a34f24119019fecc0ce
Rakesh Raut <irakeshraut@gmail.com>
2021-10-29 20:22:43 +1100

    session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_back_or_to(root_path, notice: 'Login successful')
+    else
+      flash.now[:alert] = 'Login failed'
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(login_path, notice: 'Logged out!')
+  end
+end
