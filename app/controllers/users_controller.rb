d1c7b8cec860e1150541b5b0bc0a96e0c5a701a2
Rakesh Raut <irakeshraut@gmail.com>
2021-11-01 12:18:04 +1100

    refactor user controller to show error message after redirect.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
-  end
+    @company = Company.new
+    @tab = params[:tab] if params[:tab]
+    @error_messages = params[:error_messages] if params[:error_messages]
+  end 
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    @company = Company.new
+    if @user.valid? && @user.save
       redirect_to root_path
     else
-      render :new
+      redirect_to new_user_path(tab: 'job_seeker', error_messages: @user.errors.full_messages)
     end
   end
 
