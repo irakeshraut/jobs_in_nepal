e724904d391cc6a8ac84c132292e969ec0eeb3d9
Rakesh Raut <irakeshraut@gmail.com>
2021-11-04 16:49:02 +1100

    method to show salary in Rupees.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,5 @@
+include ActiveSupport::NumberHelper
+
 class Job < ApplicationRecord
   STATUS = ['Active', 'Expired', 'Close'].freeze
   TYPE   = ['Full Time', 'Part Time', 'Casual', 'Contract', 'Freelance'].freeze
@@ -14,4 +16,17 @@ class Job < ApplicationRecord
   belongs_to :user
 
   has_rich_text :description
+
+  def salary
+    if min_salary.present? && max_salary.present?
+      "Rs #{number_to_human(min_salary, format: '%n%u', precision: 2, units: { thousand: 'K', million: 'M', billion: 'B' })} -
+       Rs #{number_to_human(max_salary, format: '%n%u', precision: 2, units: { thousand: 'K', million: 'M', billion: 'B' })}"
+    elsif min_salary.present? && max_salary.nil?
+      "Rs #{number_to_human(min_salary, format: '%n%u', precision: 2, units: { thousand: 'K', million: 'M', billion: 'B' })}"
+    elsif max_salary.present? && min_salary.nil?
+      "Rs #{number_to_human(max, format: '%n%u', precision: 2, units: { thousand: 'K', million: 'M', billion: 'B' })}"
+    else
+      'Salary Not Mentioned'
+    end
+  end
 end
