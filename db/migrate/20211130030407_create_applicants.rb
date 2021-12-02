ce38d3599cb4045b94e638cebf3976fd5d031e23
Rakesh Raut <irakeshraut@gmail.com>
2021-12-02 14:05:56 +1100

    create applicant table.

---
+++ db/migrate/20211130030407_create_applicants.rb
@@ -0,0 +1,11 @@
+class CreateApplicants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :applicants, id: :uuid do |t|
+      t.string :resume_name
+      t.string :cover_letter_name
+      t.references :job, type: :uuid
+      t.references :user, type: :uuid
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_051501) do
+ActiveRecord::Schema.define(version: 2021_11_30_030407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -45,6 +45,17 @@ ActiveRecord::Schema.define(version: 2021_11_16_051501) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "applicants", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "resume_name"
+    t.string "cover_letter_name"
+    t.uuid "job_id"
+    t.uuid "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["job_id"], name: "index_applicants_on_job_id"
+    t.index ["user_id"], name: "index_applicants_on_user_id"
   end
 
   create_table "companies", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
