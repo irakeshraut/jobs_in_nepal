0a0c4753e18a21ecec9593728b5ae3bd17f85ea6
Rakesh Raut <irakeshraut@gmail.com>
2021-12-04 18:08:20 +1100

    fix select 2 double rendering bug problem.

--- app/javascript/packs/select2.js
+++ app/javascript/packs/select2.js
@@ -1,5 +1,10 @@
 import $ from 'jquery';
 
+// This hack is required so select2 won't render twice
+document.addEventListener('turbolinks:before-cache', function() {   
+  $('.select2-hidden-accessible').select2('destroy'); 
+});
+
 $(document).on('turbolinks:load', function() {
   $('select').select2({
   });
