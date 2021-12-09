368aa8f6e5073ee74d88c1373f0dec6aa5fcd4e8
Rakesh Raut <irakeshraut@gmail.com>
2021-12-09 13:10:43 +1100

    helper methods for Applicant Model

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,5 +1,15 @@
 class Applicant < ApplicationRecord
+  STATUS = ['Shortlisted', 'Rejected']
+
   validates :resume_name, presence: true
   belongs_to :job
   belongs_to :user
+
+  def shortlisted?
+    status == 'Shortlisted'
+  end
+
+  def rejected?
+    status == 'Rejected'
+  end
 end
