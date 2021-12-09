4a03adb39914ef5d3438c82e2e2494816f0f0baf
Rakesh Raut <irakeshraut@gmail.com>
2021-12-09 13:10:11 +1100

    Add column for status on Applicant table.

---
+++ db/migrate/20211208044735_add_colums_to_applicants.rb
@@ -0,0 +1,5 @@
+class AddColumsToApplicants < ActiveRecord::Migration[6.0]
+  def change
+    add_column :applicants, :status, :string, limit: 30
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_045826) do
+ActiveRecord::Schema.define(version: 2021_12_08_044735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_12_03_045826) do
     t.uuid "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", limit: 30
     t.index ["job_id"], name: "index_applicants_on_job_id"
     t.index ["user_id"], name: "index_applicants_on_user_id"
   end
