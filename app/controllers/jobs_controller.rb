2bcb4f8604100104a1ac92f3ed940d72185edca8
Rakesh Raut <irakeshraut@gmail.com>
2021-12-18 16:54:28 +1100

    dashboard for Admin.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,8 +11,7 @@ class JobsController < ApplicationController
   end
 
   def show
-    # @job = Job.find(params[:id])
-    @job = Job.includes(:user).find(params[:id])
+    @job = Job.find(params[:id])
     unless @job.views.find_by(ip: request.remote_ip)
       @job.views.create(ip: request.remote_ip)
     end
