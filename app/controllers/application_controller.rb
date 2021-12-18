888f7703c52b4136a9eadf2111f2886caa374442
Rakesh Raut <irakeshraut@gmail.com>
2021-12-18 21:48:18 +1100

    before action to find top categories to display on footers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   include Pundit
   before_action :require_login
+  before_action :set_top_categories
 
   rescue_from Pundit::NotAuthorizedError do
     flash[:error] = "You are not authorized to perform this action."
     redirect_to root_path
+  end
+
+  def set_top_categories
+    @top_categories = Job.group(:category).order('count_all desc').count.keys.take(5) 
   end
 
   private
