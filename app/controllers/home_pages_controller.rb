02bcee52a62b4ab37eb69792980e2349148609af
Rakesh Raut <irakeshraut@gmail.com>
2022-01-05 16:35:38 +0545

    Add text for coming soon.

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,6 +1,6 @@
 class HomePagesController < ApplicationController
   skip_before_action :require_login, only: [:index]
-  
+
   def index
     @jobs = Job.includes(user: { company: [logo_attachment: :blob]}).active.order(created_at: :desc).limit(48)
   end
