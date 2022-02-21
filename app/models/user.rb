0707fffeb96de049afd10ef8228c62ebd73b3205
Rakesh Raut <irakeshraut@gmail.com>
2022-02-21 19:01:44 +1100

    send activation email when email is changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  before_update :setup_activation, if: -> { email_changed? }
+  after_update :send_activation_needed_email!, if: -> { previous_changes["email"].present? }
+
   validates :first_name, presence: true
   validates :last_name,  presence: true
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
