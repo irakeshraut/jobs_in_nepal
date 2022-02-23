7d08a9b7a7d418c5f18866df11e4b15b42eb1306
Rakesh Raut <irakeshraut@gmail.com>
2022-02-23 19:26:47 +1100

    redirect to same page when punit authorization fail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError do
     flash[:error] = "You are not authorized to perform this action."
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 
   def set_top_categories
