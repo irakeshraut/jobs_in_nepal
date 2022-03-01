6917a3bf04a7252eb5612111fd798d778e14b376
Rakesh Raut <irakeshraut@gmail.com>
2022-03-01 18:21:57 +1100

    delete description column from company now using rich_text_field.

---
+++ db/migrate/20220301071921_remove_column_from_companies.rb
@@ -0,0 +1,5 @@
+class RemoveColumnFromCompanies < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :companies, :description
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_051929) do
+ActiveRecord::Schema.define(version: 2022_03_01_071921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -73,7 +73,6 @@ ActiveRecord::Schema.define(version: 2022_03_01_051929) do
   create_table "companies", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "phone", null: false
-    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "url"
