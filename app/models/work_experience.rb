8d144b364455b92f809be6096829da77bc68cd66
Rakesh Raut <irakeshraut@gmail.com>
2022-03-01 17:02:14 +1100

    before save clean up finish month and finish year for work experience.

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -1,10 +1,20 @@
 class WorkExperience < ApplicationRecord
   validates :job_title, presence: true
   validates :company_name, presence: true
+  validates :still_in_role, inclusion: { in:  [true, false] }
   validates :start_month, presence: true
   validates :start_year, presence: true
   validates :finish_month, presence: true, unless: :still_in_role
   validates :finish_year, presence: true, unless: :still_in_role
 
   belongs_to :user
+
+  before_save :clean_up_work_experience_finish_time
+
+  def clean_up_work_experience_finish_time
+    if still_in_role
+      self.finish_month = nil
+      self.finish_year = nil
+    end
+  end
 end
