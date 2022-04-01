e3d9a272d18ffcf36cfde9ce2bc2801e2d47daa7
Rakesh Raut <irakeshraut@gmail.com>
2022-04-01 20:57:25 +1100

    more seo.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,6 +12,7 @@ class JobsController < ApplicationController
     @jobs = @jobs.filter_by_category(params[:category]) if params[:category].present?
     @jobs = @jobs.filter_by_location(params[:location]) if params[:location].present?
     @jobs = @jobs.paginate(page: params[:page], per_page: 30)
+    @category_count = @jobs.filter_by_category(params[:category]).count if params[:category].present?
   end
 
   def show
