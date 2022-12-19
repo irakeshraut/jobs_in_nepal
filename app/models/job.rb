2301615d155791feaf446dc8777c59ce1503caef
Rakesh Raut <irakeshraut@gmail.com>
2022-12-19 15:10:51 +1100

    Refactor Job model, reduced assignemnt branch complexity and refactored salary method

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,9 @@
-include ActiveSupport::NumberHelper
-include ActionView::Helpers::NumberHelper
+# frozen_string_literal: true
 
 class Job < ApplicationRecord
-  STATUS = ['Active', 'Expired', 'Closed'].freeze
+  STATUS = %w[Active Expired Closed].freeze
   TYPE   = ['Full Time', 'Part Time', 'Casual', 'Contract', 'Freelance'].freeze
-  JOB_TYPE = { 'Top Job' => 1, 'Hot Job' => 2, 'Normal Job' => 3 }
+  JOB_TYPE = { 'Top Job' => 1, 'Hot Job' => 2, 'Normal Job' => 3 }.freeze
 
   validates :title,           presence: true
   validates :category,        presence: true
@@ -15,30 +14,30 @@ class Job < ApplicationRecord
   validates :status,          inclusion: { in: Job::STATUS }
   validates :user,            presence: true
   validates :job_type,        presence: true
-  validates :job_type,        inclusion: { in: [1,2,3] }
+  validates :job_type,        inclusion: { in: [1, 2, 3] }
 
   belongs_to :user
   has_many :applicants, dependent: :destroy
+  has_many :views,      dependent: :destroy
+  has_many :bookmarks,  dependent: :destroy
   has_many :users, through: :applicants
-  has_many :views, dependent: :destroy
-  has_many :bookmarks, dependent: :destroy
 
   has_rich_text :description
   validates :description, presence: true
 
-  scope :active, -> { where(status: 'Active') }
-  scope :closed, -> { where(status: 'Closed') }
+  scope :active,  -> { where(status: 'Active') }
+  scope :closed,  -> { where(status: 'Closed') }
   scope :expired, -> { where(status: 'Expired') }
-  scope :filter_by_title, ->(title) { where("lower(title) like ?", "%#{title.downcase}%") }
-  scope :filter_by_category, ->(category) { where(category: category) }
-  scope :filter_by_location, ->(location) { where("lower(location) like ?", "%#{location.downcase}%") }
-  scope :filter_by_status, ->(status) { where("lower(status) like ?", "%#{status.downcase}%") }
+  scope :filter_by_title,      ->(title) { where('lower(title) like ?', "%#{title.downcase}%") }
+  scope :filter_by_category,   ->(category) { where(category:) }
+  scope :filter_by_location,   ->(location) { where('lower(location) like ?', "%#{location.downcase}%") }
+  scope :filter_by_status,     ->(status) { where('lower(status) like ?', "%#{status.downcase}%") }
   scope :created_by_employers, -> { joins(:user).where(users: { role: 'employer' }) }
-  scope :created_by_admin, -> { joins(:user).where(users: { role: 'admin' }) }
-  scope :old_jobs, -> { where('created_at < ?', 30.days.ago).where.not(status: 'Expired') }
+  scope :created_by_admin,     -> { joins(:user).where(users: { role: 'admin' }) }
+  scope :old_jobs,             -> { where('created_at < ?', 30.days.ago).where.not(status: 'Expired') }
 
-  scope :hot_jobs, -> { where(job_type: 1) }
-  scope :top_jobs, -> { where(job_type: 2) }
+  scope :hot_jobs,    -> { where(job_type: 1) }
+  scope :top_jobs,    -> { where(job_type: 2) }
   scope :normal_jobs, -> { where(job_type: 3) }
 
   def active?
@@ -54,28 +53,10 @@ class Job < ApplicationRecord
   end
 
   def salary
-    if min_salary.present? && max_salary.present?
-      "Rs #{number_to_human(min_salary, format: '%n%u', precision: 2, units: { thousand: 'K', million: 'M', billion: 'B' })} -
-       Rs #{number_to_human(max_salary, format: '%n%u', precision: 2, units: { thousand: 'K', million: 'M', billion: 'B' })}"
-    elsif min_salary.present? && max_salary.nil?
-      "Rs #{number_to_human(min_salary, format: '%n%u', precision: 2, units: { thousand: 'K', million: 'M', billion: 'B' })}"
-    elsif max_salary.present? && min_salary.nil?
-      "Rs #{number_to_human(max_salary, format: '%n%u', precision: 2, units: { thousand: 'K', million: 'M', billion: 'B' })}"
-    else
-      'Salary Not Mentioned'
-    end
-  end
+    return 'Salary Not Mentioned' if min_salary.blank? && max_salary.blank?
+    return formatted_salary if min_salary.present? && max_salary.present?
 
-  def full_salary
-    if min_salary.present? && max_salary.present?
-      "Rs  #{number_with_delimiter(min_salary)} - Rs #{number_with_delimiter(max_salary)}"
-    elsif min_salary.present? && max_salary.nil?
-      "Rs #{number_with_delimiter(min_salary)}"
-    elsif max_salary.present? && min_salary.nil?
-      "Rs #{number_with_delimiter(max_salary)}"
-    else
-      'Salary Not Mentioned'
-    end
+    salary_without_zero(min_salary.presence || max_salary.presence)
   end
 
   def created_by_admin?
@@ -87,12 +68,19 @@ class Job < ApplicationRecord
   end
 
   def similar_jobs
-    jobs = Job.where("lower(title) like ?", self.title.downcase).or(Job.where("lower(category) like?", self.category.downcase))
-      .where.not(id: self.id).active.order(created_at: :desc).limit(4)
-    created_by_employer = jobs.joins(:user).where(users: { role: 'employer' })
-    if created_by_employer.present?
-      jobs = jobs.includes(user: { company: [logo_attachment: :blob] })
-    end
-    jobs
+    Job.includes(user: { company: [logo_attachment: :blob] })
+       .where('lower(title) like :title OR lower(category) like :category',
+              title: title.downcase, category: category.downcase)
+       .where.not(id:).active.order(created_at: :desc).limit(4)
+  end
+
+  private
+
+  def salary_without_zero(salary)
+    number_to_currency(salary)&.gsub(/\.00$/, '')
+  end
+
+  def formatted_salary
+    "#{salary_without_zero(min_salary)} - #{salary_without_zero(max_salary)}"
   end
 end
