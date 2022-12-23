08f5b284e68ed6175011c14acf0835b91b4460a0
Rakesh Raut <irakeshraut@gmail.com>
2022-12-23 22:41:43 +1100

    - Cleanup password_resets_controller.rb

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,9 @@
 
 class PasswordResetsController < ApplicationController
   skip_before_action :require_login
+  before_action :set_token, only: %i[edit update]
+  before_action :set_user, only: %i[edit update]
+  before_action :authenticate_user, only: %i[edit update]
 
   def new; end
 
@@ -12,25 +15,30 @@ class PasswordResetsController < ApplicationController
     redirect_to(root_path)
   end
 
-  def edit
-    @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
-
-    not_authenticated if @user.blank?
-  end
+  def edit; end
 
   def update
-    @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
-
-    not_authenticated and return if @user.blank?
-
     @user.password_confirmation = params[:password_confirmation]
+
     if @user.change_password(params[:password])
       flash[:success] = 'Password successfully updated.'
       redirect_to(root_path)
     else
       render :edit
     end
+  end
+
+  private
+
+  def set_token
+    @token = params[:id]
+  end
+
+  def set_user
+    @user = User.load_from_reset_password_token(params[:id])
+  end
+
+  def authenticate_user
+    not_authenticated and return if @user.blank?
   end
 end
